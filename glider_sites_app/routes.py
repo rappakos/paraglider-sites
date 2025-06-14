9e22c84916bdc2851d42c9f9bfb3cf822fde5e26
rappakos <rappakos@gmail.com>
2025-06-14 16:49:10 +0200

    Glider sites - start adding details

--- glider_sites_app/routes.py
+++ glider_sites_app/routes.py
@@ -1,9 +1,11 @@
 import pathlib
 
-from .views import index
+from .views import index,site_details,load_flights
 
 PROJECT_ROOT = pathlib.Path(__file__).parent
 
 
 def setup_routes(app):
     app.router.add_get('/', index)
+    app.router.add_get('/{site_name}',site_details, name='site_details')
+    app.router.add_post('/{site_name}', load_flights, name='load_flights')
--- glider_sites_app/views.py
+++ glider_sites_app/views.py
@@ -6,11 +6,35 @@ from . import db
 MIN_DATE = '2018-01-01'
 
 
-def redirect(router, route_name, org = None):
-    location = router[route_name].url_for(org=org)
+def redirect(router, route_name, site_name = None):
+    location = router[route_name].url_for(site_name=site_name)
     return web.HTTPFound(location)
 
 @aiohttp_jinja2.template('index.html')
 async def index(request):
     return {'data': (await db.get_stats()).to_dict('records')}
  
+@aiohttp_jinja2.template('site.html')
+async def site_details(request):
+    site_name = request.match_info.get('site_name', None)
+    # lazy
+    all = await db.get_stats()
+    current = all[all['site_name']==site_name]
+
+    return {
+            'name':site_name,
+            'data': current.to_dict('records'),
+            'flights': []
+    }
+
+
+async def load_flights(request):
+    site_name = request.match_info.get('site_name', None)    
+    if request.method == 'POST':
+
+        print('TODO implement')
+
+
+        raise redirect(request.app.router, 'site_details', site_name=site_name)
+    else:
+        raise NotImplementedError("invalid?")

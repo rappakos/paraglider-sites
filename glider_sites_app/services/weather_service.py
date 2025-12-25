05e6c4197687eac79e1c9642c5f10adaa5e4c8f4
rappakos <rappakos@gmail.com>
2025-12-25 14:13:38 +0100

    Glider sites - adjust archive and forecast versions

---
+++ glider_sites_app/services/weather_service.py
@@ -0,0 +1,44 @@
+import logging
+from datetime import datetime, timedelta
+from glider_sites_app.repositories.sites_repository import get_stats
+from glider_sites_app.tools.weather.openmeteo_loader import refresh_weather_data
+
+MIN_DATE = '2018-01-01'
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def get_end_date(start_date:str) -> str:
+    end_date_obj = min(
+            datetime.strptime(start_date,'%Y-%m-%d') + timedelta(days=365),
+            datetime.now() + timedelta(days=-5)
+    )
+    return end_date_obj.strftime('%Y-%m-%d')
+
+
+async def sync_weather(site_name:str):
+    infos = await get_stats()
+    logger.info(infos.head())
+
+    site_info = infos[infos['site_name'] == site_name]
+    if site_info.empty:
+        logger.error(f"Site {site_name} not found in stats")
+        return
+
+    lat, lng, elev = site_info.iloc[0]['geo_latitude'], site_info.iloc[0]['geo_longitude'], site_info.iloc[0]['elevation']
+    start_date = MIN_DATE # TODO extend get_stats() with last_weather
+    end_date = get_end_date(start_date)
+
+    df = await refresh_weather_data(lat, lng, elev, start_date, end_date)
+
+    # TODO save
+    logger.info(df.head())
+
+    return df
+
+
+if __name__=='__main__':
+    import asyncio
+    asyncio.run(sync_weather('Rammelsberg NW'))
--- glider_sites_app/tools/weather/openmeteo_loader.py
+++ glider_sites_app/tools/weather/openmeteo_loader.py
@@ -9,26 +9,45 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-URL = "https://historical-forecast-api.open-meteo.com/v1/forecast" 
+URL_ARCHIVE = "https://archive-api.open-meteo.com/v1/archive"  # PRE 2022-01-01 - does not include pressure level data
+URL = "https://historical-forecast-api.open-meteo.com/v1/forecast"  # FROM 2022-01-01, include pressure level data
+
 
 TIME_ZONE = "Europe/Berlin"
 START_HOUR = 10
 END_HOUR = 18
-HOURLY_PARAMS = ["temperature_2m", 
-                 "dew_point_2m", 
-                 "precipitation", 
-                 "weather_code", 
-                 "cloud_cover_low",   # Detects Stratus/Fog                 
-                 "surface_pressure", 
-                 "wind_speed_10m", 
-                 "wind_direction_10m", 
-                 "wind_gusts_10m", 
-                 "sunshine_duration", 
+
+HOURLY_PARAMS_ARCHIVE = [
+        "temperature_2m",
+        "dew_point_2m",
+        "precipitation",
+        "weather_code",
+        "cloud_cover_low",
+        "surface_pressure", 
+        "wind_speed_10m", 
+        "wind_direction_10m", 
+        "wind_gusts_10m", 
+        "sunshine_duration",
+        "boundary_layer_height",
+        "direct_radiation",
+        "diffuse_radiation"
+]
+
+HOURLY_PARAMS = ["temperature_2m", # in archive
+                 "dew_point_2m", # in archive
+                 "precipitation", # in archive
+                 "weather_code", # in archive
+                 "cloud_cover_low",   # Detects Stratus/Fog ,  in archive              
+                 "surface_pressure",  # in archive
+                 "wind_speed_10m",  # in archive
+                 "wind_direction_10m",  # in archive
+                 "wind_gusts_10m",  # in archive
+                 "sunshine_duration",  # in archive
+                 "boundary_layer_height",  # in archive
+                 "direct_radiation",  # in archive
+                 "diffuse_radiation", #  Detects Cirrus/Haze ,  in archive 
                  "cape", 
-                 "lifted_index", 
-                 "boundary_layer_height", 
-                 "direct_radiation", 
-                 "diffuse_radiation", #  Detects Cirrus/Haze
+                 "lifted_index",                  
                  "temperature_950hPa",
                  "temperature_850hPa",
                  "wind_speed_950hPa",
@@ -42,16 +61,18 @@ async def refresh_weather_data(geo_lat: float,geo_long:float, elevation:float, s
         start_date = '2018-01-01'
         end_date = '2018-12-31'
 
+    use_archive = end_date < '2022-01-01'
+
     params = {
 	    "latitude": geo_lat,
 	    "longitude": geo_long,
         "elevation": elevation,
 	    "start_date": start_date,
 	    "end_date": end_date,
-	    "hourly": HOURLY_PARAMS,
+	    "hourly": HOURLY_PARAMS_ARCHIVE if use_archive else HOURLY_PARAMS,
     }
 
-    openmeteo_url = f'{URL}?latitude={params["latitude"]}&longitude={params["longitude"]}&elevation={params["elevation"]}&start_date={params["start_date"]}&end_date={params["end_date"]}&hourly={",".join(params["hourly"])}&timezone={TIME_ZONE}'
+    openmeteo_url = f'{URL_ARCHIVE if use_archive else URL}?latitude={params["latitude"]}&longitude={params["longitude"]}&elevation={params["elevation"]}&start_date={params["start_date"]}&end_date={params["end_date"]}&hourly={",".join(params["hourly"])}&timezone={TIME_ZONE}'
     res = requests.get(openmeteo_url)
     if res.status_code == 200:
         data = res.json()
@@ -69,5 +90,9 @@ if __name__ == "__main__":
     import asyncio
     # test - Rammi
     rammi_nw = SiteBase(site_name='Rammelsberg NW', dhv_site_id=9427, geo_latitude=51.889874886365874, geo_longitude=10.43097291843072, elevation=610 )
-    weather_data = asyncio.run(refresh_weather_data(rammi_nw.geo_latitude, rammi_nw.geo_longitude, rammi_nw.elevation, '2025-05-01', '2025-05-31'))
+    d = '2018-01-01'
+    weather_data = asyncio.run(refresh_weather_data(rammi_nw.geo_latitude, rammi_nw.geo_longitude, rammi_nw.elevation, d,d))
+    print(weather_data.tail(10))    
+    d = '2022-01-01'
+    weather_data = asyncio.run(refresh_weather_data(rammi_nw.geo_latitude, rammi_nw.geo_longitude, rammi_nw.elevation, d,d))
     print(weather_data.tail(10))
